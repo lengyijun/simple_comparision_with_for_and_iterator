63fe01299404c44baa6f59c20f7dbcd0b87583cc
Your Name <you@example.com>
2020-11-23 17:10:26 +0800

    while

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,19 @@ mod tests {
         b.iter(|| {
             let mut a: Vec<usize> = vec![0; N];
             for i in 0..N {
-                a[i] = i;
+                a[i] = i + 1;
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_use_while(b: &mut Bencher) {
+        b.iter(|| {
+            let mut index_a: usize = 0;
+            let mut cache: Vec<usize> = vec![0; N];
+            while index_a < N {
+                index_a += 1;
+                cache[index_a - 1] = index_a;
             }
         });
     }
